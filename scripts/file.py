e6feb42e8a0c9eb559b75c064c12511414e54a41
Martim <martimmourao@gmail.com>
2022-05-08 23:32:44 +0100

    Primeiro exercicio feito

--- scripts/file.py
+++ scripts/file.py
@@ -4,26 +4,14 @@ f = open(FILE)
 
 
 bat01Charge = 0
-bat02Charge = 0
 
-bat01DisCharge = 0
-bat02DisCharge = 0
 
 data = json.load(f)
 
 for cycle in data:
     if(data[cycle]["battery_ID"] == "1" and data[cycle]["type"] == "charge"):
         bat01Charge = bat01Charge + 1
-    if(data[cycle]["battery_ID"] == "1" and data[cycle]["type"] == "discharge"):
-        bat01DisCharge = bat01DisCharge + 1
-    if(data[cycle]["battery_ID"] == "2" and data[cycle]["type"] == "charge"):
-        bat02Charge = bat02Charge + 1
-    if(data[cycle]["battery_ID"] == "2" and data[cycle]["type"] == "discharge"):
-        bat02DisCharge = bat02DisCharge + 1
 
-msg.bat01Charge = bat01Charge
-msg.bat02Charge = bat02Charge
-msg.bat01DisCharge = bat01DisCharge
-msg.bat02DisCharge = bat02DisCharge
 
-return msg
+newMsg = { "payload": bat01Charge }
+return newMsg
